ce3d4674f3d472a3c361f4c9642601fbdf12ad88
Josh Pollock <jpollock412@gmail.com>
2022-03-23 17:41:01 -0400

    Update args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
+echo "Token $1"
+echo "Path $2"
 echo ::set-output name=time::$time
+
+
+npm i plugin-machine -g
+#plugin-machine plugin build --buildDir=output --pluginDir="${{ github.workspace }}" --token="$env:PLUGIN_MACHINE_TOKEN"
+#plugin-machine plugin zip --buildDir=output --pluginDir="${{ github.workspace }}" --token="$env:PLUGIN_MACHINE_TOKEN"
