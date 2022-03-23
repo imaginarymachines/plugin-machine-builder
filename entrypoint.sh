a7e481ef8018699ec53e3b593f70cf01cffc7d21
Josh Pollock <jpollock412@gmail.com>
2022-03-23 18:45:20 -0400

    Also upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,25 @@
 #!/bin/sh -l
 
+# Setup variables
 TOKEN=$1
-PLUGINDIR="/home/runner/work/plugin-machine-builder/builder-action-test-plugin"
+PLUGINDIR=$2
+ZIPNAME="builder-action-test-plugin.zip"
+UPLOAD=0;
+
 echo "Token $TOKEN"
 echo "PLUGINDIR $PLUGINDIR"
 echo ::set-output name=time::$time
 
-
+# Install
 npm i plugin-machine -g
+## Build and ZIP
 plugin-machine plugin build --buildDir=output --pluginDir="$PLUGINDIR" --token="$TOKEN"
 plugin-machine plugin zip --buildDir=output --pluginDir="$PLUGINDIR" --token="$TOKEN"
+
+## Upload to API
+echo "Uploading $ZIPNAME"
+curl --location --request POST 'https://pluginmachine.app/api/v1/files/' \
+--header 'Authorization: Bearer "$TOKEN"' \
+--form 'file=@"$PLUGINDIR/$ZIPNAME"' \
+--form 'name="$ZIPNAME"' \
+--form 'private="false"'
